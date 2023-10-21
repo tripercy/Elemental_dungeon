6dba30d768619a93483f8349428d1d2b370d1bca
tripercy <percytri37@gmail.com>
2023-10-21 10:37:00 +0700

    Added Character

---
+++ app/src/main/java/cardgame/Model/Character.java
@@ -0,0 +1,68 @@
+package cardgame.Model;
+
+public class Character implements HasElement {
+    private String name;
+    private int health;
+    private int maxHealth;
+    private int mana;
+    private int maxMana;
+
+    private Element element;
+
+    public Character(String name, int health, int mana, Element element) {
+        this.name = name;
+        this.health = health;
+        this.maxHealth = health;
+        this.mana = mana;
+        this.maxMana = mana;
+        this.element = element;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getHealth() {
+        return this.health;
+    }
+
+    public void setHealth(int health) {
+        this.health = health;
+    }
+
+    public int getMaxHealth() {
+        return this.maxHealth;
+    }
+
+    public void setMaxHealth(int maxHealth) {
+        this.maxHealth = maxHealth;
+    }
+
+    public int getMana() {
+        return this.mana;
+    }
+
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    public int getMaxMana() {
+        return this.maxMana;
+    }
+
+    public void setMaxMana(int maxMana) {
+        this.maxMana = maxMana;
+    }
+
+    public Element getElement() {
+        return this.element;
+    }
+
+    public void setElement(Element element) {
+        this.element = element;
+    }
+}
