d9b1de29146cbf5bce7be6c945904178eb504221
tripercy <percytri37@gmail.com>
2023-10-21 10:37:00 +0700

    Added Card

---
+++ app/src/main/java/cardgame/Model/Card.java
@@ -0,0 +1,57 @@
+package cardgame.Model;
+
+import java.util.Queue;
+
+public class Card implements HasElement {
+    private String name;
+    private int cost;
+    private Element element;
+    private Queue<String> effects;
+
+    public Card(String name, int cost, Element element, Queue<String> effects) {
+        this.name = name;
+        this.cost = cost;
+        this.element = element;
+        this.effects = effects;
+    }
+
+    public Element getElement() {
+        return element;
+    }
+
+    public Queue<String> getEffects() {
+        return effects;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public String getEffect() {
+        return effects.poll();
+    }
+
+    public boolean hasEffect() {
+        return !effects.isEmpty();
+    }
+
+    public void setEffects(Queue<String> effects) {
+        this.effects = effects;
+    }
+
+    public void setElement(Element element) {
+        this.element = element;
+    }
+
+    public void setCost(int cost) {
+        this.cost = cost;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
